aaf7afa7f492f5d485a5ecc4f23d4a7b50e0d793
uTatsii <103516808+uTatsii@users.noreply.github.com>
2022-09-12 18:24:35 +0000

    Deploying to gh-pages from @ uTatsii/goit-js-hw-09@444831b08a6ffbef4fb49e0a37cf28c346844c00 🚀

--- 01-color-switcher.ceffedbb.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("button[data-start]"),t=document.querySelector("button[data-stop]"),d=document.querySelector("body");let a=null;function l(){!1===e.disabled?e.disabled=!0:e.disabled=!1,!0===t.disabled?t.disabled=!1:t.disabled=!0}e.disabled=!1,t.disabled=!0,e.addEventListener("click",(function(){l(),a=setInterval((()=>{d.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),t.addEventListener("click",(function(){l(),clearInterval(a)}));
-//# sourceMappingURL=01-color-switcher.ceffedbb.js.map
